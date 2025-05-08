a4a8351d1e1d1de6039272ac9ab2656aa696d9f1
DEBASHIS NAYAK <cdnayak2000@gmail.com>
2025-05-08 15:04:18 +0530

    code for today

---
+++ 08_05_2025_Class/Activity_2/dynamic-background.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dynamic background</title>
+    <style>
+        .body-1{
+            background-color: red;
+        }
+        .body-2{
+            background-color: blue;
+        }
+        .body-3{
+            background-color: green;
+        }
+        .body-4{
+            background-color: yellow;
+        }
+        .body-5{
+            background-color: pink;
+        }
+        .body-6{
+            background-color: purple;
+        }
+    </style>
+</head>
+<!-- <body class="<?php echo 'body-'.rand(1,6); ?>">
+    <h1>This is Dynamic background color</h1>
+</body> -->
+<!-- <body class="body-"<?php echo rand(1,6);?>>
+    <h1>This is Dynamic background color</h1>
+</body> -->
+
+</html>
---
+++ 08_05_2025_Class/Activity_3/include/start.php
@@ -0,0 +1,1 @@
+<?php include 'other.php' ?>
